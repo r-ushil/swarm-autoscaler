0aa325814130b4fcdc0210ebfff6b6975a703c00
Rushil Patel <rushil.patel791@gmail.com>
2024-02-18 18:02:14 +0000

    feat: added threshold CLI argument

--- userspace-collection/inspect-cgroup.go
+++ userspace-collection/inspect-cgroup.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -14,7 +15,32 @@ const (
 	collectionPeriod = 5 * time.Second // Adjust the collection period as needed
 )
 
+
+type MemoryThreshold struct {
+	Value int64
+	Unit  string // "MB" or "GB"
+}
+
+
 func main() {
+
+	// Parse CLI arguments
+
+	mbPtr := flag.Int("mm", 0, "Memory threshold in MB")
+	gbPtr := flag.Int("mg", 0, "Memory threshold in GB")
+	flag.Parse()
+
+	var threshold MemoryThreshold
+	if *mbPtr > 0 {
+		threshold = MemoryThreshold{Value: int64(*mbPtr), Unit: "MB"}
+	} else if *gbPtr > 0 {
+		threshold = MemoryThreshold{Value: int64(*gbPtr), Unit: "GB"}
+	} else {
+		fmt.Println("Please provide a memory threshold using the -mm or -mg flag")
+		os.Exit(1)
+	}
+
+
 	files, err := os.ReadDir(cgroupDir)
 	if err != nil {
 		panic(err)
@@ -23,7 +49,7 @@ func main() {
 	for _, file := range files {
 		if strings.HasPrefix(file.Name(), "docker-") && strings.HasSuffix(file.Name(), ".scope") {
 			containerID := strings.TrimPrefix(strings.TrimSuffix(file.Name(), ".scope"), "docker-")
-			go monitorMemoryUsage(containerID)
+			go monitorMemoryUsage(containerID, threshold)
 		}
 	}
 
@@ -31,7 +57,7 @@ func main() {
 	select {}
 }
 
-func monitorMemoryUsage(containerID string) {
+func monitorMemoryUsage(containerID string, threshold MemoryThreshold) {
 	for range time.Tick(collectionPeriod) {
 		memUsage, err := readMemoryUsage(containerID)
 		if err != nil {
@@ -39,7 +65,14 @@ func monitorMemoryUsage(containerID string) {
 			continue
 		}
 
-		fmt.Printf("ContainerID: %s, Memory Usage: %d bytes\n", containerID, memUsage)
+		memUsageConverted, memUnit := convertMemoryUsage(memUsage, threshold.Unit)
+		
+		status := "under"
+		if int64(memUsageConverted) > threshold.Value {
+			status = "over"
+		}
+
+		fmt.Printf("ContainerID: %s, Status: %s memory threshold, Memory Used: %.2f %s\n", containerID, status, memUsageConverted, memUnit)
 	}
 }
 
@@ -51,4 +84,16 @@ func readMemoryUsage(containerID string) (int64, error) {
 	}
 
 	return strconv.ParseInt(strings.TrimSpace(string(content)), 10, 64)
+}
+
+
+func convertMemoryUsage(memUsage int64, unit string) (float64, string) {
+	switch unit {
+	case "MB":
+		return float64(memUsage) / 1024 / 1024, "MB"
+	case "GB":
+		return float64(memUsage) / 1024 / 1024 / 1024, "GB"
+	default:
+		return float64(memUsage), "B"
+	}
 }
