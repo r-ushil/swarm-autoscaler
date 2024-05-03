489332d48ef90054c0dbb0777c2371b473171dd6
r-ushil <rushil.patel20@imperial.ac.uk>
2024-05-03 18:33:17 +0100

    chore: vm configs

--- autoscaler/debug.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd cgroup_monitoring # go mod replace uses relative paths annoyingly
-sudo ${GOPATH}/bin/dlv debug inspect-cgroup.go -- -lower-cpu 5 -upper-cpu 80 -collection-period 3s -iface "wlp60s0"
--- autoscaler/run.sh
+++ autoscaler/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+docker stack deploy -c docker-compose.yml webserver
 cd cgroup_monitoring # go mod replace uses relative paths annoyingly
 sudo go run inspect-cgroup.go -config "../config.yaml"
 
